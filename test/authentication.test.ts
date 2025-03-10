70bb0b9dbbd73e0ddcdc8a659acdc0ff75031617
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-03-10 00:25:58 +0000

    Unittest: Added Authentication Testing

---
+++ test/authentication.test.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from "express";
+import authenticate from "../src/api/v1/middleware/authentication";
+import { auth } from "../config/firebaseConfig";
+import { AuthenticationError } from "../src/api/v1/errors/errors";
+
+describe("Testing Authentication Middleware", () => {
+    let mockRequest: Partial<Request>;
+    let mockResponse: Partial<Response>;
+    let nextFunction: jest.Mock;
+
+    beforeEach(() => {
+        mockRequest = {
+            headers: {},
+        };
+        mockResponse = {
+            locals: {},
+        };
+        nextFunction = jest.fn();
+    });
+
+    it("Should call next passing authenticationError when no token is provided", async () => {
+        // Assemble
+        const expectedError: AuthenticationError = new AuthenticationError(
+            "Unauthorized: No token provided",
+            "TOKEN_NOT_FOUND"
+        );
+
+        await authenticate(
+            mockRequest as Request,
+            mockResponse as Response,
+            nextFunction
+        );
+
+        expect(nextFunction).toHaveBeenCalledWith(expectedError);
+    });
+
+    it("should call next passing authenticationError when malformed token is provided", async () => {
+        // Assemble
+        mockRequest.headers = {
+            authorization: "Bearer ",
+        };
+
+        const expectedError: AuthenticationError = new AuthenticationError(
+            "Unauthorized: No token provided",
+            "TOKEN_NOT_FOUND"
+        );
+
+        await authenticate(
+            mockRequest as Request,
+            mockResponse as Response,
+            nextFunction
+        );
+
+        expect(nextFunction).toHaveBeenCalledWith(expectedError);
+    });
+
+    it("should call next() when token is valid", async () => {
+        // Assemble
+        mockRequest.headers = {
+            authorization: "Bearer mock-token",
+        };
+
+        // mock the google function "verifyIdToken"
+        (auth.verifyIdToken as jest.Mock).mockResolvedValueOnce({
+            uid: "mock-uid",
+            role: "user",
+        });
+
+        // Act
+        await authenticate(
+            mockRequest as Request,
+            mockResponse as Response,
+            nextFunction
+        );
+
+        expect(auth.verifyIdToken).toHaveBeenCalledWith("mock-token");
+        expect(mockResponse.locals).toEqual({
+            uid: "mock-uid",
+            role: "user",
+        });
+        expect(nextFunction).toHaveBeenCalled();
+    });
+});

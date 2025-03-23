00b25a0e4205b80feec894557e96c3bcce78589c
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-03-23 21:56:00 +0000

    modified firebaseConfig to use environment variables

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,21 +1,53 @@
-import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { initializeApp, cert, ServiceAccount, AppOptions, App, getApps } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-import { Auth, getAuth } from "firebase-admin/auth";
+import { getAuth, Auth } from "firebase-admin/auth";
 
+const getFirebaseConfig = (): AppOptions => {
+    // extract the Firebase credentials from environment variables
+    const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } =
+        process.env;
 
-const serviceAccountJSON: string | undefined = process.env.FIREBASE_KEY;
+    if (
+        !FIREBASE_PROJECT_ID ||
+        !FIREBASE_CLIENT_EMAIL ||
+        !FIREBASE_PRIVATE_KEY
+    ) {
+        // This really should be a custom error type
+        throw new Error(
+            "Missing Firebase configuaration. Please check your environment variables"
+        );
+    }
 
-if (!serviceAccountJSON){
-    throw new Error("FIREBASE_KEY env var is not met")
-}
-const serviceAccount: ServiceAccount = JSON.parse(serviceAccountJSON)
+    const serviceAccount: ServiceAccount = {
+        projectId: FIREBASE_PROJECT_ID,
+        clientEmail: FIREBASE_CLIENT_EMAIL,
+        // replace escaped newlines in the private key string with acutal newlines
+        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    };
 
-initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
-});
+    return {
+        credential: cert(serviceAccount),
+    };
+};
 
-const auth: Auth = getAuth();
+const initializeFirebaseAdmin = (): App => {
+    // check if an app is already initialized
+    const existingApp: App = getApps()[0];
 
-const db: Firestore = getFirestore();
+    if (existingApp) {
+        // return existing app if found
+        return existingApp;
+    }
+
+    // otherwise create and return new app
+    return initializeApp(getFirebaseConfig());
+};
+
+// initialize Firebase Admin app
+const app: App = initializeFirebaseAdmin();
+
+const auth: Auth = getAuth(app);
+
+const db: Firestore = getFirestore(app);
 
 export { auth, db };
